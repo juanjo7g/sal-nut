e07997544d5c35354d1aaa263ce8e4fb1a8828fa
tinez7g <tinez7g@users.noreply.github.com>
2016-03-17 15:08:52 -0500

    Update message console server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.start = function() {
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      console.log('Browse all your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
